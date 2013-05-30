8332bc337be55e1c50f4aafad63bcdb3dbf02863
dirtyoldcamel <sleipnir.norse@gmail.com>
2013-05-30 11:54:44 +0300

    Update MySql.php

--- Module/Database/Driver/MySql.php
+++ Module/Database/Driver/MySql.php
@@ -32,14 +32,14 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- * Odbc
+ * MySQL
  * 14.01.2013 20:33
  */
 namespace MOC\Module\Database\Driver;
 use MOC\Module\Database\Driver;
 
 /**
- *
+ * 
  */
 class Mysql extends Driver {
 
