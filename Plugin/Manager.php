d6da048eab07653175ba6c8e519565c079235522
Gerd Christian Kunze <gerdchristian.kunze@googlemail.com>
2013-06-05 14:26:21 +0200

    Init: Plugin-Repository (dev)

---
+++ Plugin/Manager.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ * LICENSE (BSD)
+ *
+ * Copyright (c) 2013, Gerd Christian Kunze
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ *  * Neither the name of Gerd Christian Kunze nor the names of the
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Manager
+ * 05.06.2013 13:30
+ */
+namespace MOC\Plugin;
+use MOC\Api;
+use MOC\Generic\Device\Plugin;
+/**
+ *
+ */
+class Manager implements Plugin {
+
+	/**
+	 * Get Changelog
+	 *
+	 * @static
+	 * @return \MOC\Core\Changelog
+	 * @noinspection PhpAbstractStaticMethodInspection
+	 */
+	public static function InterfaceChangelog() {
+		// TODO: Implement InterfaceChangelog() method.
+	}
+
+	/**
+	 * Get Dependencies
+	 *
+	 * @static
+	 * @return \MOC\Core\Depending
+	 * @noinspection PhpAbstractStaticMethodInspection
+	 */
+	public static function InterfaceDepending() {
+		// TODO: Implement InterfaceDepending() method.
+	}
+
+	/** @var Manager $Singleton */
+	private static $Singleton = null;
+
+	/**
+	 * Get Singleton/Instance
+	 *
+	 * @static
+	 * @return object
+	 * @noinspection PhpAbstractStaticMethodInspection
+	 */
+	public static function InterfaceInstance() {
+		if( self::$Singleton === null ) {
+			self::$Singleton = new Manager();
+		} return self::$Singleton;
+	}
+
+	/**
+	 * Load Plugin-Repository
+	 */
+	private function __construct() {
+		$Repository = Api::Module()->Drive()->Directory()->Open( __DIR__.DIRECTORY_SEPARATOR.'Repository' )->FileList();
+		foreach( $Repository as $Plugin ) {
+			$Reflection = new \ReflectionClass( 'MOC\\Plugin\\Repository\\'.$Plugin->GetName() );
+
+			var_dump( $Reflection );
+			//include_once( $Plugin->GetLocation() );
+
+			//$this->Repository
+		}
+	}
+
+	/** @var Hook[] $Repository */
+	private $Repository = array();
+
+	/**
+	 * @param null|string $PluginName
+	 */
+	public function VideoPlayer( $PluginName = null ) {
+
+	}
+}
---
+++ Plugin/Repository/FlowPlayer.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * LICENSE (BSD)
+ *
+ * Copyright (c) 2013, Gerd Christian Kunze
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ *  * Neither the name of Gerd Christian Kunze nor the names of the
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * FlowPlayer
+ * 05.06.2013 14:00
+ */
+namespace MOC\Plugin\Repository;
+use MOC\Plugin\Hook;
+/**
+ *
+ */
+class FlowPlayer extends Hook implements Hook\VideoPlayer {
+	/**
+	 * This method must be used to setup your plugin
+	 *
+	 * - called only once
+	 *
+	 * @return Hook
+	 */
+	public function HookLoader() {
+		// TODO: Implement HookLoader() method.
+	}
+
+	/**
+	 * @param int $Width  px
+	 * @param int $Height px
+	 *
+	 * @return void
+	 */
+	public function SetPlayerSize( $Width, $Height ) {
+		// TODO: Implement SetPlayerSize() method.
+	}
+
+	/**
+	 * @param string $Movie
+	 *
+	 * @return void
+	 */
+	public function SetPlayerSource( $Movie ) {
+		// TODO: Implement SetPlayerSource() method.
+	}
+
+	/**
+	 * @return string
+	 */
+	public function GetPlayer() {
+		// TODO: Implement GetPlayer() method.
+	}
+}
